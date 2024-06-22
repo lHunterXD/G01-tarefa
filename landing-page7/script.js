bf727ec39b94829b060e77c4df8d3ecd2223326f
Douglas <douglas.l2.hunter@gmail.com>
2024-06-21 23:51:17 -0300

    falta ajeitar o nav menu

---
+++ landing-page7/script.js
@@ -0,0 +1,12 @@
+function displayTime(){
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
