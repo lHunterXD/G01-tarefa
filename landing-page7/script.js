2eb58bff15e1e85e89598131d58859afaa64dc0d
Douglas <douglas.l2.hunter@gmail.com>
2024-06-23 14:03:42 -0300

    trabalho feito possiveis alteraçoes no futuro

--- landing-page7/script.js
+++ landing-page7/script.js
@@ -10,3 +10,23 @@ function displayTime(){
     document.getElementById('seconds').innerHTML = sec;
 }
 setInterval(displayTime, 10);
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
